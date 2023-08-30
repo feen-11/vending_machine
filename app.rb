a7a5dfd54e2f1d9afeaa166d6b22fa141cc8e623
taichi <taichishiozawa@gmail.com>
2023-08-30 21:29:05 +0900

    Drinkクラスを定義

--- app.rb
+++ app.rb
@@ -15,3 +15,29 @@ class Suica
   end
 
 end
+
+
+class Drink
+
+  attr_accessor :name, :price, :stock
+
+  def initialize(name='ペプシ', price=150, stock=5)
+    @name = name
+    @price = price
+    @stock = stock
+  end
+
+  def show_stock
+    self.stock
+  end
+
+end
+
+class Purchase
+
+  def initialize(sales=0)
+    @sales = sales
+  end
+
+end
+
